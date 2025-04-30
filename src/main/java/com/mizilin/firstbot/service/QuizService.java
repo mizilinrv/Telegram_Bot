46d189cee71a756b53cff3cc0dbbf6cdccceaa76
Roman <roman@mizilin.ru>
2025-04-30 14:25:34 +0300

    Добавил создание кнопки

--- src/main/java/com/mizilin/firstbot/service/QuizService.java
+++ src/main/java/com/mizilin/firstbot/service/QuizService.java
@@ -7,8 +7,6 @@ import com.mizilin.firstbot.repository.QuizRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityNotFoundException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -58,10 +56,6 @@ public class QuizService {
 
     public List<Quiz> getAllQuizzes() {
         return quizRepository.findAll();
-    }
-
-    public Quiz getQuizById(Long id) {
-        return quizRepository.findWithQuestionsAndOptionsAndResultsById(id);
     }
 
     @Transactional
